1c856e524b68da9230ac98dce2e566db45c8365a
Marcin Chrost <mchrost82@gmail.com>
2025-05-20 13:49:37 +0200

    Day 3 - methods - solutions (amendment)

--- src/main/java/eu/chrost/day2/s2methods/solutions/Task3.java
+++ src/main/java/eu/chrost/day2/s2methods/solutions/Task3.java
@@ -4,6 +4,11 @@ public class Task3 {
     public static void main(String[] args) {
         System.out.println(average(4, 7, 9));
         System.out.println(average());
+
+        double average = average();
+        if (Double.isNaN(average)) {
+            System.out.println("NaN");
+        }
     }
 
 
