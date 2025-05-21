40542b3284b0195e7bb48edd1bf1fbe35bf8f529
Marcin Chrost <mchrost82@gmail.com>
2025-05-21 11:50:34 +0200

    Day 3 - constructors & records - task 1 - solution

--- src/main/java/eu/chrost/day3/s1constructors/task1/BankAccount.java
+++ src/main/java/eu/chrost/day3/s1constructors/task1/BankAccount.java
@@ -1,4 +1,46 @@
 package eu.chrost.day3.s1constructors.task1;
 
 class BankAccount {
+
+    private final String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0.0; //mozna bez this
+    }
+
+    public BankAccount() {
+        this("0000", 0.0);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" + "accountNumber='" + accountNumber + '\'' + ", balance=" + balance + '}';
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
 }
--- src/main/java/eu/chrost/day3/s1constructors/task1/Main.java
+++ src/main/java/eu/chrost/day3/s1constructors/task1/Main.java
@@ -2,6 +2,20 @@ package eu.chrost.day3.s1constructors.task1;
 
 class Main {
     public static void main(String[] args) {
-
+        BankAccount konto1 = new BankAccount();
+        BankAccount konto2 = new BankAccount("12345");
+        BankAccount konto3 = new BankAccount("5678", 500);
+        System.out.println("Konta:");
+        System.out.println(konto1);
+        System.out.println(konto2);
+        System.out.println(konto3);
+        konto1.deposit(500);
+        System.out.println(konto1);
+        konto2.withdraw(1000);
+        System.out.println(konto2);
+        konto3.withdraw(200);
+        System.out.println(konto3);
+        System.out.println(konto1.getBalance());
+        System.out.println(konto2.getAccountNumber());
     }
 }
