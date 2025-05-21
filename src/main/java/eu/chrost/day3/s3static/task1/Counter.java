b9f84e3f8a1f08c43327c1f52247078103d50919
Marcin Chrost <mchrost82@gmail.com>
2025-05-21 14:25:06 +0200

    Day 3 - static - task 1 - solution

--- src/main/java/eu/chrost/day3/s3static/task1/Counter.java
+++ src/main/java/eu/chrost/day3/s3static/task1/Counter.java
@@ -1,4 +1,26 @@
 package eu.chrost.day3.s3static.task1;
 
 class Counter {
+
+    private static int objectCount;
+
+    private final int id;
+
+    public static int getObjectCount(){
+        return objectCount;
+    }
+
+    public Counter() {
+        objectCount++;
+        id = objectCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Counter{" +
+                "id=" + id +
+                '}';
+    }
+
+
 }
